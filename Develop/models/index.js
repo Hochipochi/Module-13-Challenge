9efaf9d7802d4b6a17c8585faa92ceba0e607c6e
Michael Smitte <smittemichael@yahoo.com>
2022-12-13 19:45:05 -0500

    imported sequelize in server.js, turned on the server, and added routes to index.js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,31 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'categories_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  foreignKey: {
+    model: 'ProductTag',
+    key: 'id',
+  },
+}),
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  foreignKey: {
+    model: 'ProductTag',
+    key: 'id',
+  },
+}),
 
 module.exports = {
   Product,
